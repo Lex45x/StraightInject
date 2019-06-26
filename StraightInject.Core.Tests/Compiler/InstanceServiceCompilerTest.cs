ee66ffac3279dfd006abb4baa596713409848598
Alex Redka <alex@ebrains.space>
2019-06-26 23:36:17 +0300

    Extract test class to separate file

--- StraightInject.Core.Tests/Compiler/InstanceServiceCompilerTest.cs
+++ StraightInject.Core.Tests/Compiler/InstanceServiceCompilerTest.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Reflection.Emit;
 using NUnit.Framework;
 using StraightInject.Core.Compilers;
-using StraightInject.Core.ConstructorResolver;
 using StraightInject.Core.ServiceConstructors;
 using StraightInject.Core.Services;
 using StraightInject.Core.Tests.Services;
@@ -37,40 +36,6 @@ namespace StraightInject.Core.Tests.Compiler
 
             Assert.IsNotNull(plainService);
             Assert.AreSame(plainService, instance);
-        }
-    }
-    
-    [TestFixture]
-    public class SingletonServiceCompilerTest
-    {
-        [Test]
-        public void CompilationTest()
-        {
-            Environment.SetEnvironmentVariable("STRAIGHT_INJECT_ENABLE_DIAGNOSTIC", "true");
-
-            var compiler = new SingletonServiceCompiler();
-            var stubCompiler = new DynamicAssemblyJumpTableOfTypeHandleContainerCompiler(new Dictionary<Type, IServiceCompiler>
-            {
-                [typeof(SingletonService)] = compiler
-            });
-
-            var interfaceService = new SingletonService(typeof(PlainService), new EagerConstructorResolver(), typeof(IPlainService));
-            var classService = new SingletonService(typeof(PlainService), new EagerConstructorResolver(), typeof(PlainService));
-
-            var dependencies = new Dictionary<Type, IService>
-            {
-                [typeof(IPlainService)] = interfaceService,
-                [typeof(PlainService)] = classService
-            };
-
-            var container = stubCompiler.CompileDependencies(dependencies);
-
-            var plainServiceInterface = container.Resolve<IPlainService>();
-            var plainService = container.Resolve<PlainService>();
-
-            Assert.IsNotNull(plainService);
-            Assert.IsNotNull(plainServiceInterface);
-            Assert.AreSame(plainService, plainServiceInterface);
         }
     }
 }
---
+++ StraightInject.Core.Tests/Compiler/SingletonServiceCompilerTest.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+using StraightInject.Core.Compilers;
+using StraightInject.Core.ConstructorResolver;
+using StraightInject.Core.ServiceConstructors;
+using StraightInject.Core.Services;
+using StraightInject.Core.Tests.Services;
+using StraightInject.Services;
+
+namespace StraightInject.Core.Tests.Compiler
+{
+    [TestFixture]
+    public class SingletonServiceCompilerTest
+    {
+        [Test]
+        public void CompilationTest()
+        {
+            Environment.SetEnvironmentVariable("STRAIGHT_INJECT_ENABLE_DIAGNOSTIC", "true");
+
+            var compiler = new SingletonServiceCompiler();
+            var stubCompiler = new DynamicAssemblyJumpTableOfTypeHandleContainerCompiler(
+                new Dictionary<Type, IServiceCompiler>
+                {
+                    [typeof(SingletonService)] = compiler
+                });
+
+            var interfaceService = new SingletonService(typeof(PlainService), new EagerConstructorResolver(),
+                typeof(IPlainService));
+            var classService = new SingletonService(typeof(PlainService), new EagerConstructorResolver(),
+                typeof(PlainService));
+
+            var dependencies = new Dictionary<Type, IService>
+            {
+                [typeof(IPlainService)] = interfaceService,
+                [typeof(PlainService)] = classService
+            };
+
+            var container = stubCompiler.CompileDependencies(dependencies);
+
+            var plainServiceInterface = container.Resolve<IPlainService>();
+            var plainService = container.Resolve<PlainService>();
+
+            Assert.IsNotNull(plainService);
+            Assert.IsNotNull(plainServiceInterface);
+            Assert.AreSame(plainService, plainServiceInterface);
+        }
+    }
+}
