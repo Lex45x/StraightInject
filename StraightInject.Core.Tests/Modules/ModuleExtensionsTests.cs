58948b44cd5f2bbc48121410e3ffdd325792f5b8
Olexiy Kulchitskiy <olexiy.kulchitskiy@adraba.io>
2019-07-10 22:59:43 +0300

    Added extension to wrap registration into modules

---
+++ StraightInject.Core.Tests/Modules/ModuleExtensionsTests.cs
@@ -0,0 +1,38 @@
+﻿using NUnit.Framework;
+using StraightInject.Core.Extensions;
+using StraightInject.Core.Tests.Services;
+
+namespace StraightInject.Core.Tests.Modules
+{
+    public class ModuleExtensionsTests
+    {
+        private IDependencyMapper _mapper;
+
+        [SetUp]
+        public void Setup()
+        {
+            _mapper = DefaultDependencyComposer.Initialize();
+        }
+
+        [Test]
+        public void Compile_WithModuleUsage_Success()
+        {
+            Assert.DoesNotThrow(() =>
+            {
+                _mapper.FromModule<TestModule>();
+                var container = _mapper.Compile();
+                var dependentService = container.Resolve<IDependentService>();
+                Assert.IsInstanceOf<DependentService>(dependentService);
+            });
+        }
+    }
+
+    public class TestModule : IModule
+    {
+        public void Apply(IDependencyMapper dependencyMapper)
+        {
+            dependencyMapper.FromType<DependentService>().ToService<IDependentService>();
+            dependencyMapper.FromType<DependencyService>().ToService<IDependencyService>();
+        }
+    }
+}
